2ae1ed68725eae24695ac5293d0d94f52f086342
shoreditchdesign <webflow@shoreditchdesignstudio.com>
2024-06-13 12:45:32 +0100

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -48,3 +48,44 @@ setTimeout(() => {
     gsap.set("[a-stagger-up]", { opacity: 1 });
 }, 1800); // 1800ms delay for preloader animation
 });
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Delay execution for preloader animation
+    setTimeout(() => {
+      // Ensure GSAP and plugins are loaded
+      if (typeof gsap === "undefined" || typeof ScrollTrigger === "undefined" || typeof SplitType === "undefined") {
+        console.error("GSAP, ScrollTrigger, or SplitType is not loaded.");
+        return;
+      }
+  
+      // Register ScrollTrigger plugin with GSAP
+      gsap.registerPlugin(ScrollTrigger);
+  
+      // Link timelines to scroll position
+      function createScrollTrigger(triggerElement, timeline) {
+        // Play tl when scrolled into view (60% from top of screen), only once
+        ScrollTrigger.create({
+          trigger: triggerElement,
+          start: "top 60%",
+          onEnter: () => timeline.play(),
+          once: true // Ensures the animation runs only once
+        });
+      }
+  
+      document.querySelectorAll("[a-fade-left]").forEach((element) => {
+        let tl = gsap.timeline({ paused: true });
+        tl.from(element.querySelectorAll(".word"), {
+          opacity: 0,
+          x: -30, // 30px offset from the left
+          duration: 0.5,
+          ease: "power1.in", // ease-in curve
+          stagger: 0.05, // Stagger each element by 50 ms
+        });
+        createScrollTrigger(element, tl);
+      });
+  
+      // Avoid flash of unstyled content
+      gsap.set("[a-fade-left]", { opacity: 1 });
+    }, 1800); // 1800ms delay for preloader animation
+  });
+  
