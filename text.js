fa90cafc9a5f0f660e7fc53e6851c859d496d7c9
shoreditchdesign <webflow@shoreditchdesignstudio.com>
2024-07-01 20:14:24 +0100

    Update text.js

--- text.js
+++ text.js
@@ -55,7 +55,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         });
 
         // Avoid flash of unstyled content
-        gsap.set("[a-stagger-up]", { opacity: 0 });
+        // gsap.set("[a-stagger-up]", { opacity: 0 });
 
       
     }, 0); // 1000ms delay for preloader animation
