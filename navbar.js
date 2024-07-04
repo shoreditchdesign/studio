54e1ab0cf85c907137d08beed812a8ae7e3cc424
shoreditchdesign <webflow@shoreditchdesignstudio.com>
2024-07-04 22:08:31 +0100

    Added navbar.js

---
+++ navbar.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector('.c-navbar');
+var prevScrollpos = window.pageYOffset;
+const navbarHeight = navbar.offsetHeight; // Get navbar height
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-" + navbarHeight + "px";
+  }
+  prevScrollpos = currentScrollPos;
+}
