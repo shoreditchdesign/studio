bebbac22d0f211bbcc3a673886b9cedbfdb1e7ca
Shoreditch Design <developer@shoreditchdesignstudio.com>
2024-07-07 14:22:10 +0100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,9 +1,9 @@
 const navbar = document.querySelector('.c-navbar');
-var prevScrollpos = window.pageYOffset;
+var prevScrollpos = window.scrollY;
 const navbarHeight = navbar.offsetHeight; // Get navbar height test 1
 
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+  var currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
