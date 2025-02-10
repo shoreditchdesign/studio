1e30c63147d6ebd32d7f69cd1cf86166ec9bef4b
shoreditchdesign <webflow@shoreditchdesignstudio.com>
2025-02-10 17:10:27 +0400

    Delete cssnavbar.js

--- cssnavbar.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('[data-nav-element="menu"]').addEventListener('click', () => {
-    const navbar = document.querySelector('[data-nav-element="navbar"]');
-    const currentState = navbar.getAttribute('data-nav-state');
-    navbar.setAttribute('data-nav-state', currentState === 'open' ? 'closed' : 'open');
-  });
